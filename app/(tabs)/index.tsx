a82926d6e4151fc46f5443983bfd26d737484477
umarluqman <umarluqman.78@gmail.com>
2024-05-20 12:11:41 +0800

    ini

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,122 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, View, useWindowDimensions } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { BubblePacking } from "@/components/BubblePacking";
+import { GestureHandler } from "@/components/GestureHandler";
+import { utxos } from "@/data";
+import { useGestures } from "@/hooks/useGestures";
+import { useImageLayout } from "@/hooks/useImageLayout";
+import { Canvas } from "@shopify/react-native-skia";
+import { hierarchy, pack } from "d3";
+import React, { useMemo, useState } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export enum Keychain {
+  Internal,
+  External,
+}
+
+export interface Utxo {
+  txid: string;
+  vout: number;
+  value: number;
+  timestamp?: Date;
+  label?: string;
+  addressTo?: string;
+  keychain: Keychain;
+}
+
+export interface UtxoListBubble {
+  id: string;
+  value: number;
+  children: UtxoListBubble[];
+}
+
+export const outpoint = (u: Utxo) => `${u.txid}:${u.vout}`;
 
 export default function HomeScreen() {
+  const { width, height } = useWindowDimensions();
+
+  const GRAPH_HEIGHT = height;
+  const GRAPH_WIDTH = width;
+
+  const canvasSize = { width: GRAPH_WIDTH, height: GRAPH_HEIGHT };
+
+  console.log({ utxos });
+  const utxoList = utxos.map((data) => {
+    return {
+      id: outpoint(data),
+      children: [],
+      value: data.value,
+    };
+  });
+
+  const utxoPack = useMemo(() => {
+    const utxoHierarchy = () =>
+      hierarchy<UtxoListBubble>({
+        id: "root",
+        children: utxoList,
+        value: utxoList.reduce((acc, cur) => acc + cur.value, 0),
+      })
+        .sum((d) => d?.value ?? 0)
+        .sort((a, b) => (b?.value ?? 0) - (a?.value ?? 0));
+
+    const createPack = pack<UtxoListBubble>()
+      .size([GRAPH_WIDTH, GRAPH_HEIGHT])
+      .padding(4);
+
+    return createPack(utxoHierarchy()).leaves();
+  }, [utxoList]);
+
+  const { width: w, height: h, center, onImageLayout } = useImageLayout({});
+  const { animatedStyle, gestures, transform } = useGestures({
+    width: w,
+    height: h,
+    center,
+    isDoubleTapEnabled: true,
+    maxPanPointers: 2,
+    minPanPointers: 1,
+    maxScale: 50,
+    minScale: 0.1,
+  });
+
+  const [selectedCircle, setSelectedCircle] = useState<string[]>([]);
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={[styles.container]}>
+        <Canvas
+          style={[
+            {
+              ...canvasSize,
+              borderWidth: 1,
+              borderColor: "red",
+            },
+          ]}
+          onLayout={onImageLayout}
+        >
+          <BubblePacking
+            transform={transform}
+            selectedCircle={selectedCircle}
+            utxoPack={utxoPack}
+            canvasSize={canvasSize}
+          />
+        </Canvas>
+        <GestureHandler
+          contentContainerAnimatedStyle={animatedStyle}
+          canvasSize={canvasSize}
+          onLayoutContent={onImageLayout}
+          selectedCircle={selectedCircle}
+          setSelectedCircle={setSelectedCircle}
+          bubblePack={utxoPack}
+          zoomGesture={gestures}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    backgroundColor: "#131313",
   },
 });
